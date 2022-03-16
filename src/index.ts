f48ab2d0e1fe922582ad9d36bd96d79855942a9d
hadi chloun <hadi_960@hotmail.com>
2022-03-16 13:17:15 -0500

    wrapped the middlerware in function

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql"
+
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
+const server = new ApolloServer({ schema });
 
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send("failed to delete listing");
-});
-app.listen(port);
+server.start().then(() => {
+    server.applyMiddleware({ app, path: "/api" })
+    app.listen(port);
+})
 
 console.log(`[app]: http://localhost:${port}`);
+
+
