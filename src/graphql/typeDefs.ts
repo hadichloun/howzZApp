d98205cd579726b27c463b6c8be02823ffa19f36
hadi chloun <hadi_960@hotmail.com>
2022-03-17 11:39:47 -0500

    fetched is working and fixed the IResolvers

---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,23 @@
+import { gql } from "apollo-server-express"
+
+export const typeDefs = gql`
+type Listing {
+    id: String!
+    title: String!
+    image: String!
+    address: String!
+    price: Int!
+    numOfGuests: Int!
+    numOfBeds: Int!
+    numberOfBaths: Int!
+    rating: Int!
+}
+
+type Query {
+    listings: [Listing!]!
+}
+
+type Mutation {
+    deleteListing(id: ID!): Listing!
+}
+`
